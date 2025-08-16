467b81e0c2465c21669024681d91b49b1575ee97
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2025-08-16 12:49:00 +0200

    refactor

--- generate.js
+++ generate.js
@@ -1,19 +1,27 @@
-import { Note, noteTypes } from "@anotes/core";
-import { capitalize } from "../text.js";
-import { readDatabase } from "./db.js";
-import { md } from "./markdown.js";
-import { Decl, Tags } from "./tags.js";
+import { formatNotes, Note, NoteList, noteTypes } from "@anotes/core";
+import { writeFile } from "node:fs/promises";
+import { readDatabase } from "./lib/database.js";
+import { pathTo } from "./lib/io.js";
+import { md } from "./lib/markdown.js";
+import { Decl, Tags } from "./lib/tags.js";
+import { capitalize } from "./lib/text.js";
 
-async function generateExamples(notes) {
-  const db = await readDatabase();
-  genitiveCase(db, notes);
-  dativeCase(db, notes);
-  accusativeCase(db, notes);
-  instrumentalCase(db, notes);
-  locativeCase(db, notes);
+const db = await readDatabase();
+const notes = new NoteList();
+for (const [przypadek, generator] of [
+  ["dopełniacz", dopełniacz],
+  ["celownik", celownik],
+  ["biernik", biernik],
+  ["narzędnik", narzędnik],
+  ["miejscownik", miejscownik],
+]) {
+  generator(db, notes);
 }
+const file = pathTo(`notes/odmiany/rzeczowniki.note`);
+console.log(`Generated ${notes.length} note(s) to file "${file}"`);
+await writeFile(file, formatNotes(notes));
 
-function genitiveCase(db, notes) {
+function dopełniacz(db, notes) {
   // szukać, uczyć, słuchać, używać, życzyć, potrzebować (kogo czego)
   const words = expand([
     ["szukać", [["dobry|zły|taki", ["człowiek", "kobieta", "dziecko"]]]],
@@ -49,16 +57,16 @@ Dopełniacz (*kogo? czego?*)
           const a = `${md.b(Agen.form)} ${md.b(Ngen.form)}`;
           const front = capitalize(`${V.form} ${q}.`);
           const back = md.p(capitalize(`${V.form} ${a}.`)) + help;
-          const id = `genitive_${V.form}_${Anom.form}_${Nnom.form}`;
+          const id = `dopełniacz_${V.form}_${Anom.form}_${Nnom.form}`;
           addNote(notes, id, front, back);
-          console.log(`Genitive case: ${V.form} (${Anom.form}->${Agen.form}) (${Nnom.form}->${Ngen.form})`);
+          console.log(`Dopełniacz: ${V.form} (${Anom.form}->${Agen.form}) (${Nnom.form}->${Ngen.form})`);
         }
       }
     }
   }
 }
 
-function dativeCase(db, notes) {
+function celownik(db, notes) {
   const words = expand([
     ["dawać", [["dobry|zły|taki", ["człowiek", "kobieta", "dziecko"]]]],
     ["pomagać", [["ważny|miły|taki", ["nauczyciel", "nauczycielka", "dziecko"]]]],
@@ -86,16 +94,16 @@ Celownik (*komu? czemu?*)
           const a = `${md.b(Adat.form)} ${md.b(Ndat.form)}`;
           const front = capitalize(`${V.form} ${q}.`);
           const back = md.p(capitalize(`${V.form} ${a}.`)) + help;
-          const id = `dative_${V.form}_${Anom.form}_${Nnom.form}`;
+          const id = `celownik_${V.form}_${Anom.form}_${Nnom.form}`;
           addNote(notes, id, front, back);
-          console.log(`Dative case: ${V.form} (${Anom.form}->${Adat.form}) (${Nnom.form}->${Ndat.form})`);
+          console.log(`Celownik: ${V.form} (${Anom.form}->${Adat.form}) (${Nnom.form}->${Ndat.form})`);
         }
       }
     }
   }
 }
 
-function accusativeCase(db, notes) {
+function biernik(db, notes) {
   // widzieć, czytać, kupować, lubić (kogo co)
   const words = expand([
     ["lubić", [["dobry|zły|taki", ["człowiek", "brat", "kobieta", "siostra", "dziecko"]]]],
@@ -126,16 +134,16 @@ Biernik (*kogo? co?*)
           const a = `${md.b(Aacc.form)} ${md.b(Nacc.form)}`;
           const front = capitalize(`${V.form} ${q}.`);
           const back = md.p(capitalize(`${V.form} ${a}.`)) + help;
-          const id = `accusative_${V.form}_${Anom.form}_${Nnom.form}`;
+          const id = `biernik_${V.form}_${Anom.form}_${Nnom.form}`;
           addNote(notes, id, front, back);
-          console.log(`Accusative case: ${V.form} (${Anom.form}->${Aacc.form}) (${Nnom.form}->${Nacc.form})`);
+          console.log(`Biernik: ${V.form} (${Anom.form}->${Aacc.form}) (${Nnom.form}->${Nacc.form})`);
         }
       }
     }
   }
 }
 
-function instrumentalCase(db, notes) {
+function narzędnik(db, notes) {
   const words = expand([
     ["rozmawiać+z", [["dobry|zły|taki", ["człowiek", "kobieta", "dziecko", "nauczyciel", "nauczycielka"]]]],
     ["interesować+się", [["silny|słaby|taki", ["człowiek", "kobieta", "dziecko", "nauczyciel", "nauczycielka"]]]],
@@ -164,16 +172,16 @@ Narzędnik (*z kim? z czym?*)
           const cverb = conn(V.form, verb);
           const front = capitalize(`${cverb} ${q}.`);
           const back = md.p(capitalize(`${cverb} ${a}.`)) + help;
-          const id = `instrumental_${V.form}_${Anom.form}_${Nnom.form}`;
+          const id = `narzędnik_${V.form}_${Anom.form}_${Nnom.form}`;
           addNote(notes, id, front, back);
-          console.log(`Instrumental case: ${cverb} (${Anom.form}->${Ainst.form}) (${Nnom.form}->${Ninst.form})`);
+          console.log(`Narzędnik: ${cverb} (${Anom.form}->${Ainst.form}) (${Nnom.form}->${Ninst.form})`);
         }
       }
     }
   }
 }
 
-function locativeCase(db, notes) {
+function miejscownik(db, notes) {
   const words = expand([
     ["mówić+o", [["dobry|zły|taki", ["człowiek", "kobieta", "dziecko", "nauczyciel", "nauczycielka"]]]],
     ["pisać+o|czytać+o", [["silny|słaby|taki", ["człowiek", "kobieta", "dziecko", "nauczyciel", "nauczycielka"]]]],
@@ -202,9 +210,9 @@ Miejscownik (*o kim? o czym?*)
           const cverb = conn(V.form, verb);
           const front = capitalize(`${cverb} ${q}.`);
           const back = md.p(capitalize(`${cverb} ${a}.`)) + help;
-          const id = `locative_${V.form}_${Anom.form}_${Nnom.form}`;
+          const id = `miejscownik_${V.form}_${Anom.form}_${Nnom.form}`;
           addNote(notes, id, front, back);
-          console.log(`Locative case: ${cverb} (${Anom.form}->${Aloc.form}) (${Nnom.form}->${Nloc.form})`);
+          console.log(`Miejscownik: ${cverb} (${Anom.form}->${Aloc.form}) (${Nnom.form}->${Nloc.form})`);
         }
       }
     }
@@ -258,5 +266,3 @@ function conn(form, word) {
   }
   return form + " " + word.substring(i + 1);
 }
-
-export { generateExamples };
